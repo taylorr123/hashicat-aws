5cbf57ca2ea77e0521006b0fd7a7666bdd9b0f5d
taylor ryzuk <ryzuk.taylor@gmail.com>
2021-03-19 20:05:11 +0000

    adding s3 bucket

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,6 +1,6 @@
 module "s3-bucket" {
   source  = "app.terraform.io/TaylorRyzuk-training/s3-bucket/aws"
   version = "1.15.0"
-  bucket_prefix = "${env.prefix}" 
   # insert required variables here
+  bucket_prefix = "${var.prefix}"
 }
--- variables.tf
+++ variables.tf
@@ -9,6 +9,7 @@ variable "prefix" {
   description = "This prefix will be included in the name of most resources."
 }
 
+
 variable "region" {
   description = "The region where the resources are created."
   default     = "us-east-1"
