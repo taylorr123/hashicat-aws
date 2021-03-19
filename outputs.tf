c6e8878373557d85ecbec09c94abc13f13c94f63
taylor ryzuk <ryzuk.taylor@gmail.com>
2021-03-19 20:24:42 +0000

    s3 output

--- outputs.tf
+++ outputs.tf
@@ -7,6 +7,7 @@ output "catapp_ip" {
   value = "http://${aws_eip.hashicat.public_ip}"
 }
 
-output "s3_bucket_url" {
-  value = "${module.s3-bucket.this_s3_bucket_website_endpoint}"
+output "this_s3_bucket_website_endpoint" {
+  description = "The website endpoint, if the bucket is configured with a website. If not, this will be an empty string."
+  value       = element(concat(aws_s3_bucket.this.*.website_endpoint, [""]), 0)
 }
